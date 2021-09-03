a6c7790294b89820b1ac0d0c866f2594c5084574
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-09-04 00:39:00 +0300

    feature/library created
    feature/library created (but not finished)

--- src/js/api/refs.js
+++
@@ -1,9 +0,0 @@
-export default function getRefs() {
-  return {
-    searchForm: document.querySelector('.search-form'),
-
-    // libraryBtn: document.querySelector('.storage__input'),
-    // libraryWatchedBtn: document.getElementById('js-WatchedButton'),
-    // libraryQueuedBtn: document.getElementById('js-QueueButton'),
-  };
-}
--- src/js/library.js
+++ src/js/library.js
@@ -1,37 +1,105 @@
 // import filmListTemplate from './'
-import getRefs from './api/refs';
+import refs from './refs';
+import { notice, error } from '@pnotify/core';
 
-const libraryBox = document.querySelector('.gallery');
-const libraryWatchedBtn = getRefs().libraryWatchedBtn;
-const libraryQueuedBtn = getRefs().libraryQueuedBtn;
-let watchedFilmStorage = [];
-let queueFilmStorage = [];
+refs.myLibraryLink.addEventListener('click', onLibraryBtn);
+refs.watchedBtn.addEventListener('click', onLibraryWachedBtm);
+refs.queueBtn.addEventListener('click', onLibraryQueueBtn);
+refs.addQueueBtn.addEventListener('click', onAddQueueBtn);
+refs.addWatchedBtn.addEventListener('click', onAddWachedBtm);
+
+const watchedFilmStorage = JSON.parse(localStorage.getItem('watche-films'));
+const queueFilmStorage = JSON.parse(localStorage.getItem('queue-films'));
+
+function onLibraryWachedBtm(e) {
+  refs.myLibraryLink.innerHTML = filmListTemplate(watchedFilmStorage);
+}
+function onLibraryQueueBtn(e) {
+  refs.myLibraryLink.innerHTML = filmListTemplate(queueFilmStorage);
+}
+
 function onLibraryBtn(e) {
-  if (libraryWatchedBtn.checked) {
-// Если кнопка Watched - checked выполняем этот код
-    if (watchedFilmStorage !== [] || '') {
-      libraryBox.innerHTML = filmListTemplate(watchedFilmStorage);
-    } else {
-      libraryBox.innerHTML = '';
-    }
-    // watchedFilmStorage !== [] || ''
-    //   ? (libraryBox.innerHTML = filmListTemplate(watchedFilmStorage))
-    //   : (libraryBox.innerHTML = '');
-    }
-    if (libraryQueuedBtn.checked) {
-// Если кнопка Watched - checked выполняем этот код
-        if (queueFilmStorage !== [] || '') {
-      libraryBox.innerHTML = filmListTemplate(queueFilmStorage);
-    } else {
-      libraryBox.innerHTML = '';
-    }
-    
-        
-function onWachedBtm(e) {
-  localStorage.setItem('watche-films', JSON.stringify(watchedFilmStorage));
+  if (refs.watchedBtn.checked) {
+    // Если кнопка Watched в состоянии checked - выполняем этот код
+
+    // if (watchedFilmStorage !== [] || '') {
+    //   onWachedBtm();
+    // } else {
+    //    refs.myLibraryLink.innerHTML = '';
+    // noticeMessage.notice();
+    // }
+    watchedFilmStorage !== [] || ''
+      ? (refs.myLibraryLink.innerHTML = onLibraryWachedBtm())
+      : (refs.myLibraryLink.innerHTML = ''),
+      noticeMessage.notice();
+  }
+  if (refs.queueBtn.checked) {
+    // Если кнопка Queue в состоянии checked - выполняем этот код
+
+    // if (queueFilmStorage !== [] || '') {
+    //   //  refs.myLibraryLink.innerHTML = filmListTemplate(queueFilmStorage);
+    //   onQueueBtn();
+    // } else {
+    //    refs.myLibraryLink.innerHTML = '';
+    // noticeMessage.notice();
+    // }
+    queueFilmStorage !== [] || ''
+      ? (refs.myLibraryLink.innerHTML = onQueueBtn())
+      : (refs.myLibraryLink.innerHTML = ''),
+      noticeMessage.notice();
+  }
 }
-function onQueueBtn(e) {
-  localStorage.setItem('queue-films', JSON.stringify(queueFilmStorage));
+
+function onAddWachedBtm() {
+  if (addWachedBtm.classList.contains('remove')) {
+    onRemoveFromWatchedBtm();
+  } else {
+    localStorage.setItem('watche-films', JSON.stringify(watchedFilmStorage));
+    addWachedBtm.classList.toggle('remove');
+    addWachedBtm.textContent('Remove from Wached');
+  }
 }
-function onRemoveFromWatchedBtm(e) {}
-function onRemoveFromQueueBtm(e) {}
+function onAddQueueBtn() {
+  if (refs.addQueueBtn.classList.contains('remove')) {
+    onRemoveFromQueueBtm();
+  } else {
+    localStorage.setItem('queue-films', JSON.stringify(queueFilmStorage));
+    refs.addQueueBtn.classList.toggle('remove');
+    refs.addQueueBtn.textContent('Remove from Queue');
+  }
+}
+function onRemoveFromWatchedBtm() {
+  localStorage.removeItem('watche-films');
+  addWachedBtm.classList.toggle('remove');
+  addWachedBtm.textContent('Add to Wached');
+}
+function onRemoveFromQueueBtm() {
+  localStorage.removeItem('queue-films');
+  refs.addQueueBtn.classList.toggle('remove');
+  refs.addQueueBtn.textContent('Add to Queue');
+}
+
+class NoticeMessage {
+  constructor() {}
+  notice() {
+    notice({
+      // title: 'Attention',
+      text: 'Your Library is empty',
+      width: '300px',
+      minHeight: '15px',
+      delay: 2000,
+      // addClass: 'error',
+    });
+  }
+  error() {
+    error({
+      title: 'Error',
+      text: 'No matchs found!',
+      width: '300px',
+      minHeight: '15px',
+      delay: 2000,
+      // addClass: 'error',
+    });
+  }
+}
+const noticeMessage = new NoticeMessage();

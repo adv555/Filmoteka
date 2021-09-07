fc9541c7b0fa35b5922b09cf778d14727180269b
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-09-07 18:08:55 +0300

    add folder components

---
+++ src/js/components/lightbox.js
@@ -0,0 +1,9 @@
+// import * as basicLightbox from 'basiclightbox';
+// // // ======== show modal
+
+// export default function showModal(template, opts = {}) {
+//   const instance = basicLightbox.create(template);
+
+//   instance.show();
+//   // instance.element().querySelector('.basicLightbox__placeholder').onclick = instance.close;
+// }

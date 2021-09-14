3bbd7d3040c093eec7b6d5230e9251bdd3156c8b
Andrew Yatsutko <eitherwiser@gmail.com>
2021-09-14 13:53:21 +0300

    opent modal btn bagfix

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,6 @@
 import refs from './refs';
 import MoviesApiService from './api/api-service';
-import { trailerTemplate, getDataMovieById } from './modal-card'
+import { trailerTemplate, getDataMovieById, getMovieIdAndMarkupCardMovie } from './modal-card'
 import swiper from '../templates/swiper'
 
 
@@ -8,39 +8,18 @@ const moviesApiService = new MoviesApiService();
 moviesApiService.fetchUpcomingMovies().then(renderUpcomingMovies)
 
 //=========== get content =========== 
-//let UpcomingCollection = [];
-
-
 
 function renderUpcomingMovies(data) {
   refs.filmStrip.innerHTML = '';
-  //let UpcomingCollection = [...data.results];
   localStorage.setItem('UpcomingCollection', JSON.stringify([...data.results]));
-  //refs.filmStrip.innerHTML = swiper(UpcomingCollection);
   refs.filmStrip.innerHTML = swiper(JSON.parse(localStorage.getItem('UpcomingCollection')));
-
-  //document.querySelector('.slider-card-markup').innerHTML = libraryCardMarkup(UpcomingCollection);
   sliderActions();
 }
 
+//============= init slider =============
 function sliderActions() {
   const slider = new UpcomingCollectionEngine(document.querySelector('[data-component="slider"]'));
   const container = document.querySelectorAll('.switcher-wrapper');
-
-  document.querySelector('.arrow__previous').addEventListener('click', () => {
-    slider.next()
-  })
-  document.querySelector('.arrow__next').addEventListener('click', () => {
-    slider.previous()
-  })
-
-
-  //const filmStrip = document.querySelect('.film-strip');
-
-  //filmStrip.forEach(item => {
-  //  item.setAttribute('data-position', '{{@index}}');
-  //});
-
 
   // ============= autoplay slider =============
   let deactivateTimeout;
@@ -59,25 +38,31 @@ function sliderActions() {
 
   // ============= film-strip navigation =============
 
-
+  //=== arrows ===
+  document.querySelector('.arrow__previous').addEventListener('click', () => {
+    slider.next()
+  })
+  document.querySelector('.arrow__next').addEventListener('click', () => {
+    slider.previous()
+  })
 
   document.querySelectorAll('.film-strip__item').forEach(item => {
     item.addEventListener('click', () => {
       const position = Number(item.getAttribute("data-position"));
-      console.log('hello')
       slider.jump(position);
     })
   })
 
-  //======== film btns ============
+  //=== film-strip btns ===
+
   document.querySelectorAll('.film-strip__item')
     .forEach(item => {
-      //item.addEventListener('click', handle_click);
+
       const id = item.getAttribute('data-source')
 
-      item.querySelector('.film-strip__info').addEventListener('click', () => {
-        getDataMovieById(id);
-        localStorage.setItem('currentCardID', id);
+      item.querySelector('.false-image').addEventListener('click', () => {
+        //getDataMovieById(id);
+        getMovieIdAndMarkupCardMovie();
       });
 
       item.querySelector('.film-strip__play-video').addEventListener('click', () => {
@@ -91,46 +76,32 @@ function sliderActions() {
 
       item.querySelector('.film-strip__add-to-lib').addEventListener('click', () => {
         console.log(id);
-        localStorage.setItem('currentCardID', id); //write current movie ID
-        function getMoviefromLS(filmsLS) {
-          let arr = JSON.parse(localStorage.getItem(filmsLS));
-          console.log((arr.filter(function (item, i, arr) {
-            return (item.id == localStorage.getItem('currentCardID'));
-          })))
-        };
 
-        if (getMoviefromLS('queue-films') !== id) {
-
-        } return;
-
-        console.log(getMoviefromLS('galleryCardList'))
-        console.log(getMoviefromLS('galleryCardList'))
-        let arr = JSON.parse(localStorage.getItem('galleryCardList'));
-        console.log((arr.filter(function (item, i, arr) {
-          return (item.id == localStorage.getItem('currentCardID'));
-        })))
+        //function getMoviefromLS(filmsLS) {
+        //  let arr = JSON.parse(localStorage.getItem(filmsLS));
+        //  console.log((arr.filter(function (item, i, arr) {
+        //    return (item.id == localStorage.getItem('currentCardID'));
+        //  })))
+        //};
+        //if (getMoviefromLS('queue-films') !== id) {
+        //} return;
+        //console.log(getMoviefromLS('galleryCardList'))
+        //console.log(getMoviefromLS('galleryCardList'))
+        //let arr = JSON.parse(localStorage.getItem('galleryCardList'));
+        //console.log((arr.filter(function (item, i, arr) {
+        //  return (item.id == localStorage.getItem('currentCardID'));
+        //})))
 
       });
 
     });
-
-
-
-  //function handle_click(event) {
-  //  console.log(event)
-  //console.log(event.currentTarget.parentElement.getAttribute('data-source'));
-  //getMovieId(event.currentTarget.parentElement.getAttribute('data-source'));
-  //}
-
 }
 
 
-export default class UpcomingCollectionEngine {
-
+class UpcomingCollectionEngine {
 
   constructor() {
     this.items = [...document.querySelectorAll('[data-slider]')];
-    //this.items = [...document.querySelectorAll('[data-position]')];
     this.length = this.items.length - 1;
     this.autoPlay();
   };
@@ -163,6 +134,7 @@ export default class UpcomingCollectionEngine {
     }
   };
 
+  //=== autoPlay ====
 
   autoPlay() {
     if (this.autoPlayInterval) {
@@ -184,45 +156,4 @@ export default class UpcomingCollectionEngine {
     this.active = true;
   }
 
-
 };
-
-
-  //=========== Autoplay slider ===========
-  //  var sliderOnPause = false;
-  //  let pauseInterval = time + 3000;
-  //  let intervalTimer = time + 500;
-  //  let time = 0;
-
-  //  function getCurrentTime() {
-  //    return Date.now();
-  //  }
-
-  //  function SliderDelay() {
-  //    sliderOnPause = true;
-  //    let timer = getCurrentTime() + pauseInterval;
-  //    let count = 0;
-  //    if (timer > count) {
-  //      count = getCurrentTime();
-  //    }
-  //    sliderOnPause = false;
-  //  }
-
-  //  function startSliderAutoPlay() {
-  //    timer = getCurrentTime() + intervalTimer;
-  //    let count = 0;
-  //    if (timer > count) {
-  //      count = getCurrentTime();
-  //    }
-  //    if (sliderOnPause = false)
-  //      count = getCurrentTime();
-  //  }
-  //  this.slide('next')
-  //  startSliderAutoPlay();
-  //}
-  //startSliderAutoPlay();
-
-
-
-
-
